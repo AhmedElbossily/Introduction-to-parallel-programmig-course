ac16332217569690bfa492e3f3bb48a3113c9ef7
AhmedElbossily <ahmed.fathy.elbossily@gmail.com>
2023-01-12 18:07:12 +0200

    updating run file

--- run.sh
+++ run.sh
@@ -6,18 +6,21 @@
 #PBS  -o /home/ghada/SPH_GPU/C-out.txt
 #PBS  -e /home/ghada/SPH_GPU/C-error.txt
 
+
+export PATH="/cm/shared/apps/gcc9/9.3.0/bin/:$PATH}" 
+export PATH="/usr/include/glm/:$PATH}" 
+export PATH="/cm/local/apps/cmake-gcc9/3.21.3/bin/:$PATH}"
+export PATH="/cm/shared/apps/opencv4-py37-cuda11.2-gcc8/4.5.4/bin/:$PATH}"
+
+
 module load shared
 module load gcc9/9.3.0
 module load cmake-gcc9/3.21.3  
 module load cuda11.2/toolkit/11.2.2
 module load cuda11.2/blas/11.2.2
 module load cuda11.2/fft/11.2.2
-#export PATH="/cm/local/apps/cmake-gcc9/3.21.3/bin/:$PATH}"
+module load opencv4-py37-cuda11.2-gcc8/4.5.4
 
-#cd $PBS_O_WORKDIR
-
-#cd /home/ghada/SPH_GPU/build/
-#./Mfree
 
 rm -rf build
 mkdir build
