c74c030318e73a8f9bf7c65bc8a5df28c8ceed35
AhmedElbossily <ahmed.fathy.elbossily@gmail.com>
2023-01-12 18:13:14 +0200

    updating run

--- run.sh
+++ run.sh
@@ -15,12 +15,13 @@ export PATH="/cm/shared/apps/opencv4-py37-cuda11.2-gcc8/4.5.4/bin/:$PATH}"
 
 module load shared
 
-module load opencv4-py37-cuda11.2-gcc8/4.5.4
 module load openblas/dynamic/0.3.7 
 module load hdf5_18/1.8.21 gcc8/8.4.0 
 module load python37 
 module load cudnn8.1-cuda11.2/8.1.1.33 
 module load ml-pythondeps-py37-cuda11.2-gcc8/4.7.8
+module load opencv4-py37-cuda11.2-gcc8/4.5.4
+
 
 module load gcc9/9.3.0
 module load cmake-gcc9/3.21.3  
